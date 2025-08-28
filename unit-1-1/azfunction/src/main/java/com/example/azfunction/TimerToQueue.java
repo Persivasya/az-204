1e29d0b96bcc4c4ffa3e9631364f1ba0e5e28199
Andrey Chekotovsky <achekotovsky@ibagroup.eu>
2025-08-28 15:36:38 +0200

    init

---
+++ unit-1-1/azfunction/src/main/java/com/example/azfunction/TimerToQueue.java
@@ -0,0 +1,17 @@
+package com.example.azfunction;
+
+import java.time.LocalDateTime;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+
+public class TimerToQueue {
+    @FunctionName("TimerToQueue")
+    public void run(
+        @TimerTrigger(name = "timerInfo", schedule = "0 * * * * *") String timerInfo,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+    }
+}
