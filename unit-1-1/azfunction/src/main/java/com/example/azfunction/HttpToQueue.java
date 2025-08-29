38f42a7486ae6b4dbcc61571dfad2acf8b216408
Persivasya <andreychekotovsky@proton.me>
2025-08-29 17:24:14 +0200

    Added Http function

--- unit-1-1/azfunction/src/main/java/com/example/azfunction/HttpToQueue.java
+++ unit-1-1/azfunction/src/main/java/com/example/azfunction/HttpToQueue.java
@@ -6,16 +6,15 @@ import java.util.Optional;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 
 public class HttpToQueue {
-    @FunctionName("HttpTrigger")
+    @FunctionName("HttpToQueue")
     public void run(
-        @com.microsoft.azure.functions.annotation.HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
         final ExecutionContext context
     ) {
         context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
