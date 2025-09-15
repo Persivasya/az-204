8c20ac260480ec2a27c4569227fbbec887a34c92
Persivasya <andreychekotovsky@proton.me>
2025-09-15 12:04:01 +0200

    Created blob trigger

---
+++ unit-1-1/azfunction/src/main/java/com/example/azfunction/BlobTruggerToQueue.java
@@ -0,0 +1,21 @@
+package com.example.azfunction;
+
+import java.time.LocalDateTime;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.BlobTrigger;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.QueueOutput;
+
+public class BlobTruggerToQueue {
+    @FunctionName("BlobTruggerToQueue")
+    public void run(
+        @BlobTrigger(name = "file", path = "mycontainer/{name}", connection = "AzureWebJobsStorage") byte[] content,
+        @QueueOutput(name = "output", queueName = "output", connection = "AzureWebJobsStorage") OutputBinding<byte[]> output,
+        final ExecutionContext context
+    ) {
+        output.setValue(content);
+        context.getLogger().info("Java Blob trigger function executed at: " + LocalDateTime.now());
+    }
+}
