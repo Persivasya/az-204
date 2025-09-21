22d3c4df9b263cbc4c93272f95b26b3b99c92ff0
Persivasya <andreychekotovsky@proton.me>
2025-09-21 15:28:23 +0200

    Added logging for hashes

--- unit-1-1/azfunction/src/main/java/com/example/azfunction/gitwebhook/GitEventLoggerFunction.java
+++ unit-1-1/azfunction/src/main/java/com/example/azfunction/gitwebhook/GitEventLoggerFunction.java
@@ -31,7 +31,8 @@ public class GitEventLoggerFunction {
         String signature256 = request.getHeaders().getOrDefault("X-Hub-Signature-256", "");
         String secret = System.getenv("GIT_SECRET_KEY");
         if (secret.isEmpty() || signature.isBlank()
-                || !verifyGithubSignature(request.getBody().orElse(""), signature256, signature, secret, true)) {
+                || !verifyGithubSignature(request.getBody().orElse(""), signature256, signature, secret, true,
+                        context)) {
             return request.createResponseBuilder(HttpStatus.UNAUTHORIZED).body("Unauthorized").build();
         }
 
@@ -43,19 +44,23 @@ public class GitEventLoggerFunction {
     }
 
     static boolean verifyGithubSignature(String body, String sig256, String sig1, String secret,
-            boolean allowSha1Fallback) {
+            boolean allowSha1Fallback, ExecutionContext context) {
         if (secret == null || secret.isEmpty())
             return false;
 
         // Prefer SHA-256
         if (sig256 != null && sig256.startsWith("sha256=")) {
             String expected = "sha256=" + hmacHex("HmacSHA256", secret, body);
+            context.getLogger().info("Expected: " + expected);
+            context.getLogger().info("Sig256: " + sig256);
             return constantTimeEquals(expected, sig256);
         }
 
         // Legacy SHA-1 only if you explicitly allow it
         if (allowSha1Fallback && sig1 != null && sig1.startsWith("sha1=")) {
             String expected = "sha1=" + hmacHex("HmacSHA1", secret, body);
+            context.getLogger().info("Expected: " + expected);
+            context.getLogger().info("Sig1: " + sig1);
             return constantTimeEquals(expected, sig1);
         }
         return false;
